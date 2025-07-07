b4443bb59b4e3ff219d7d4f787b58d07640f763e
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-07 17:32:21 +0900

    chore: mongodb 연결

--- app/main.py
+++ app/main.py
@@ -1,13 +1,40 @@
+from contextlib import asynccontextmanager
+
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
+from app.config.constants import CODE, MESSAGE
+from app.db.session import client, db, init_db
 from app.exception.custom_exception import AppException
 
 load_dotenv(override=True)
 
 
-app = FastAPI()
+@asynccontextmanager
+async def db_lifespan(application: FastAPI):
+    try:
+        await init_db()
+
+        ping_response = await db.command("ping")
+
+        if int(ping_response["ok"]) != 1:
+            raise AppException(
+                status=500,
+                code=CODE.ERROR.DB_CONNECTION_FAILED,
+                message=MESSAGE.ERROR.DB_CONNECTION_FAILED,
+            )
+
+        application.mongodb_client = client
+        application.database = db
+
+        yield
+
+    finally:
+        client.close()
+
+
+app = FastAPI(lifespan=db_lifespan)
 
 
 @app.get("/")
