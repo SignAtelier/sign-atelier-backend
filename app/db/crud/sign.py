d9d05ba691425e4674b09c9a41685de868c0477e
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-09 21:35:59 +0900

    feat: 싸인 목록 조회 API 구현
    - feat: 싸인 목록 응답 시 presigned url 추가
    - feat: 싸인 목록 조회 라우팅 설정
    - feat: 싸인 목록 조회 구현
    - feat: db 싸인 목록 조회 로직 구현

--- app/db/crud/sign.py
+++ app/db/crud/sign.py
@@ -5,3 +5,7 @@ async def save_sign(user, file_name: str, sign_name: str):
     sign = Sign(user=user, file_name=file_name, name=sign_name)
 
     await sign.insert()
+
+
+async def get_signs(user):
+    return await Sign.find(Sign.user.id == user.id).to_list()
