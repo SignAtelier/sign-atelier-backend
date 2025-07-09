155501c98c61b996e0215ec69fe882654cededc0
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-09 21:42:39 +0900

    feat: db 싸인 이름 수정 로직 구현

--- app/db/crud/sign.py
+++ app/db/crud/sign.py
@@ -1,3 +1,5 @@
+from bson import ObjectId
+
 from app.models.sign import Sign
 
 
@@ -9,3 +11,12 @@ async def save_sign(user, file_name: str, sign_name: str):
 
 async def get_signs(user):
     return await Sign.find(Sign.user.id == user.id).to_list()
+
+
+async def update_name(sign_id, new_name):
+    object_id = ObjectId(sign_id)
+    sign = await Sign.find_one(Sign.id == object_id)
+
+    await sign.set({Sign.name: new_name})
+
+    return sign
