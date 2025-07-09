0f8cf445f288f7071f848a3b28c80c561d823af6
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-10 03:29:06 +0900

    feat: access token 재발급 라우터 설정 및 쿠키 설정

--- app/routes/auth_router.py
+++ app/routes/auth_router.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import RedirectResponse
+from fastapi import APIRouter, Cookie, Request
+from fastapi.responses import RedirectResponse, Response
 
 from app.config.auth import oauth
-from app.config.constants import TOKEN
-from app.services.auth_service import fetch_google_userinfo, handle_google_auth
+from app.config.constants import CODE, COOKIE, MESSAGE
+from app.exception.custom_exception import AppException
+from app.services.auth_service import (
+    fetch_google_userinfo,
+    handle_google_auth,
+    reissue_access_token,
+)
 
 router = APIRouter()
 
@@ -18,18 +23,51 @@ async def login(request: Request):
 @router.get("/google", name="auth_google")
 async def auth(request: Request):
     userinfo = await fetch_google_userinfo(request)
-    access_token = await handle_google_auth(userinfo, "google")
+    tokens = await handle_google_auth(userinfo, "google")
 
     redirect_uri = "http://localhost:5173"
 
     response = RedirectResponse(url=redirect_uri)
     response.set_cookie(
         key="access_token",
+        value=tokens["access_token"],
+        httponly=True,
+        samesite="lax",
+        secure=False,
+        max_age=COOKIE.MaxAge.ACCESS,
+    )
+    response.set_cookie(
+        key="refresh_token",
+        value=tokens["refresh_token"],
+        httponly=True,
+        samesite="lax",
+        secure=False,
+        max_age=COOKIE.MaxAge.REFRESH,
+    )
+
+    return response
+
+
+@router.post("/refresh")
+async def refresh_access_token(
+    refresh_token: str = Cookie(None),
+):
+    if not refresh_token:
+        raise AppException(
+            status=401,
+            code=CODE.ERROR.MISSING_REFRESH_TOKEN,
+            message=MESSAGE.ERROR.MISSING_REFRESH_TOKEN,
+        )
+
+    access_token = await reissue_access_token(refresh_token)
+    response = Response()
+    response.set_cookie(
+        key="access_token",
         value=access_token,
         httponly=True,
         samesite="lax",
         secure=False,
-        max_age=TOKEN.EXPIRE.ACCESS,
+        max_age=COOKIE.MaxAge.ACCESS,
     )
 
     return response
